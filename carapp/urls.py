b2ac705b04f7d8a83b3e3bb3d7f1bb385cc83eb0
Md Abu Hasib Mahmud <57803694+iftymahmud@users.noreply.github.com>
2024-11-08 16:31:04 -0500

    Lab 8

--- carapp/urls.py
+++ carapp/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views
-from .views import AboutUsView, TeamMembersView
+from .views import AboutUsView, TeamMembersView, SignUpView
 
 
 app_name = 'carapp'
@@ -14,4 +14,8 @@ urlpatterns = [
     path('team/', TeamMembersView.as_view(), name='team_members'),
     path('vehicles/', views.vehicles, name='vehicles'),
     path('order/', views.orderhere, name='orderhere'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', views.login_here, name='login'),
+    path('logout/', views.logout_here, name='logout'),
+    path('myorders/', views.list_of_orders, name='myorders'),
 ]
--- carapp/views.py
+++ carapp/views.py
@@ -1,16 +1,39 @@
 # carapp/views.py
 
 from django.http import HttpResponse
-from .models import CarType, Vehicle, TeamMember
+from .models import CarType, Vehicle, TeamMember, OrderVehicle, Buyer
 from django.shortcuts import get_object_or_404
 from django.views import View
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from .models import Buyer
 
 
 def homepage(request):
     cartype_list = CarType.objects.all().order_by('id')
-    # Passing 'cartype_list' as an extra context variable to the template
-    return render(request, 'carapp/homepage.html', {'cartype_list': cartype_list})
+
+    visit_count = request.session.get('visit_count', 0)
+    request.session['visit_count'] = visit_count + 1
+
+    context = {
+        'cartype_list': cartype_list,
+        'visit_count': visit_count + 1,
+    }
+
+    response = render(request, 'carapp/homepage.html', context)
+    # Set a cookie named 'cookie_counter' that expires in 10 seconds
+    response.set_cookie('cookie_counter', 'Cookie Value', max_age=10)
+    return response
+
 
 
 
@@ -47,3 +70,61 @@ def vehicles(request):
 
 def orderhere(request):
     return HttpResponse("You can place your order here.")
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('carapp:login')
+    template_name = 'carapp/signup.html'
+
+    def form_valid(self, form):
+        # Save the User instance directly
+        response = super().form_valid(form)
+
+        # At this point, 'form.instance' is already the saved User object
+        # If you need additional fields, set them here
+        # For example:
+        user = form.instance
+        user.save()
+
+        return response
+
+
+def login_here(request):
+    if request.method == 'POST':
+        username = request.POST.get('username').strip()
+        password = request.POST.get('password').strip()
+
+        print(f"Username: {username}, Password: {password}")  # Debug
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('carapp:homepage'))
+            else:
+                return HttpResponse('Your account is disabled.')
+        else:
+            return HttpResponse('Login details are incorrect.')
+    else:
+        return render(request, 'carapp/login_here.html')
+
+@login_required
+def logout_here(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('carapp:homepage'))
+
+@login_required
+def list_of_orders(request):
+    user = request.user
+    try:
+        buyer = Buyer.objects.get(id=user.id)
+        orders = OrderVehicle.objects.filter(buyer=buyer)
+        if orders.exists():
+            return render(request, 'carapp/list_of_orders.html', {'orders': orders})
+        else:
+            message = 'You have not placed any orders.'
+            return render(request, 'carapp/list_of_orders.html', {'message': message})
+    except Buyer.DoesNotExist:
+        return HttpResponse('You are not registered.')
